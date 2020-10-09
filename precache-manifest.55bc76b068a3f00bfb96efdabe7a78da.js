4eabfcf72f427af3fc7fba5fe96cf03734a38960
Ananye Singhal <ananyesinghal@gmail.com>
2020-10-09 15:37:49 +0530

    Updates

---
+++ precache-manifest.55bc76b068a3f00bfb96efdabe7a78da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9835656df64d5a14f3a2823eaa458b",
+    "url": "/React-Project-1/index.html"
+  },
+  {
+    "revision": "9743151046992738181c",
+    "url": "/React-Project-1/static/css/main.99e75adc.chunk.css"
+  },
+  {
+    "revision": "5ef86903a0397b0951fc",
+    "url": "/React-Project-1/static/js/2.209823b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Project-1/static/js/2.209823b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9743151046992738181c",
+    "url": "/React-Project-1/static/js/main.d0816046.chunk.js"
+  },
+  {
+    "revision": "467a3ea9aa52ef70ded7",
+    "url": "/React-Project-1/static/js/runtime-main.055d22ee.js"
+  },
+  {
+    "revision": "ed44dc5d1d3df4597fbdd91513f53382",
+    "url": "/React-Project-1/static/media/video.ed44dc5d.mp4"
+  }
+]);
